0e389ab4200d30598c43a5357609c098978e7c6c
Nasadmi <98781576+Nasadmi@users.noreply.github.com>
2024-01-12 09:57:14 +0000

    Add base files :tada:

---
+++ src/main.server.ts
@@ -0,0 +1,8 @@
+import { bootstrapApplication } from '@angular/platform-browser'
+import { AppComponent } from './app/app.component'
+import { config } from './app/app.config.server'
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const bootstrap = async () => await bootstrapApplication(AppComponent, config)
+
+export default bootstrap
